858afab227bf82a4143ff663f5a439f3bc8810ee
Vlad-bit-313 <vladia313@gmail.com>
2021-07-18 21:00:45 +0700

    Добавил проверку формы логина, регистрации, а также переход на страницу

--- conftest.py
+++ conftest.py
@@ -3,6 +3,10 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+# del
+import time
+
+
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name',
@@ -35,4 +39,5 @@ def browser(request):
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nquit browser..")
+
     browser.quit()
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,8 +1,12 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
+from .locators import MainPageLocators
+from .login_page import LoginPage
 
 
 class MainPage(BasePage):
     # Тест-кейс. Проверить, что есть ссылка, которая ведет на логин
-    def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+    def go_to_login_page(self):
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+        # return LoginPage(browser=self.browser, url=self.browser.current_url)
